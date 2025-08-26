f6d76dadf029b41fc94c3558795f65c2ef94c49a
Arpit Bhomia <arpitbhomia1204@gmail.com>
2025-08-26 18:54:06 +0530

    Messager bug Solved

--- P2P_TermiChat/PrintMessage.cpp
+++ P2P_TermiChat/PrintMessage.cpp
@@ -1,10 +1,9 @@
 #include"P2P_TermiChat.h"
 
 //  for Printing the Messages
-void print_message(const string &msg) {
-    std::lock_guard<std::mutex> lock(chat_mutex);
+void print_message(const std::string &msg) {
     if (!chat_win) return;
-    waddstr(chat_win,msg.c_str()+'\n');
+    std::lock_guard<std::mutex> lock(chat_mutex);  // protect ncurses access
+    waddstr(chat_win, (msg + "\n").c_str());
     wrefresh(chat_win);
-
 }
